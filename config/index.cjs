79630ccf8961c2ba5de027a5b5b2701b562a52d5
czl56 <119572928+czl56@users.noreply.github.com>
2022-12-01 19:12:54 +0800

    index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oW9tg5ygd2awqNU-aHx71r_A0Sp8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'v3G5_70-zzg3gjucznuLIacsdqNUhPpGiBjLRY2KCEs',
+      useTemplateId: 'vBnwlAH8zbRJLUUscEAaSKBep6x1ayykE1G99ClsgCI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
